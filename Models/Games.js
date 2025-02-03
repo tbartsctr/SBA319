bdbba5308d437ae8815e1ab279f6475ca8963f4e
tbartsctr <tbartsctr@gmail.com>
2025-02-03 00:49:25 -0500

    SBA319Comm3

--- Models/Games.js
+++ Models/Games.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const gameSchema = new mongoose.Schema({
     title: {type: String, required: true},
-    genre: {type: String, required: true},
+    genre: {type: [String], required: true},
     releaseYear: {type: Number, required: true},
     platform: {type: [String], required: true},
     rating: {type: Number, min: 1, max: 10},
